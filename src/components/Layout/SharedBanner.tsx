2ac5c1cc37dfde46dbcb9418692e95fa7d1e5edf
igee-17 <igbagboleye@gmail.com>
2025-06-27 16:42:17 +0100

    dna bg changed

--- src/components/Layout/SharedBanner.tsx
+++ src/components/Layout/SharedBanner.tsx
@@ -14,7 +14,7 @@ const SharedBanner: React.FC<{
         backgroundSize: "cover",
       }}
     >
-      <div className="h-full pt-44 pb-32 bg-primaryGreen/90 ">
+      <div className="h-full pt-44 pb-32 bg-primaryGreen/70 ">
         <SlideIn direction="top" duration={800} delay={200}>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center">
