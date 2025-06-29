d1062093f73ec6916de5519ac860ef4d613f9d29
marketintelsolutions <info@ziltchone.com>
2025-06-29 15:44:30 +0100

    shared

--- src/components/Layout/SharedBanner.tsx
+++ src/components/Layout/SharedBanner.tsx
@@ -5,21 +5,21 @@ const SharedBanner: React.FC<{
   img?: string;
   heading: string;
   text: string;
-}> = ({ img = "building", heading, text }) => {
+}> = ({ img = "pagebanner", heading, text }) => {
   return (
     <section
-      className="h-[550px]  text-white"
+      className="h-[550px]  text-primaryGreen"
       style={{
         backgroundImage: `url(/images/${img}.jpg)`,
         backgroundSize: "cover",
       }}
     >
-      <div className="h-full pt-44 pb-32 bg-primaryGreen/70 ">
+      <div className="h-full pt-44 pb-32 bg-primaryGreen/20 ">
         <SlideIn direction="top" duration={800} delay={200}>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center">
               <h1 className="text-4xl lg:text-6xl font-bold mb-6">{heading}</h1>
-              <p className="text-xl text-gray-100 max-w-3xl mx-auto">{text}</p>
+              <p className="text-2xl text-black max-w-3xl mx-auto">{text}</p>
             </div>
           </div>
         </SlideIn>
